1643307922040b58c39165d7d1396907aa1022b8
ntaha <you@example.com>
2023-12-10 21:27:32 +0200

    pms-session1

---
+++ src/Components/Login/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className=' vh-100 auth-container d-flex justify-content-center align-items-center'>
+      <div className='login-wrapper w-50 h-50 bg-info'>
+      Login
+      </div>
+    </div>
+  )
+}
