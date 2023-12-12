4912b8ac96cf9426118fac43875996f4ace24981
ziadessamcr7 <ziadessamcr7@gmail.com>
2023-12-12 03:45:27 +0200

    first commit

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -4,7 +4,7 @@ export default function Login() {
   return (
     <div className=' vh-100 auth-container d-flex justify-content-center align-items-center'>
       <div className='login-wrapper w-50 h-50 bg-info'>
-      Login
+        Login
       </div>
     </div>
   )
