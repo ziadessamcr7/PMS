df0aad3d875577b0e54bfe60a8106738475b7822
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2023-12-20 12:26:27 +0200

    update navBar

--- src/Shared/MasterLayout/MasterLayout.tsx
+++ src/Shared/MasterLayout/MasterLayout.tsx
@@ -1,29 +1,31 @@
-import React from 'react'
-import SideBar from '../SideBar/SideBar'
-import NavBar from '../Navbar/NavBar'
-import { Outlet } from 'react-router-dom'
+/** @format */
+
+import React from "react";
+import SideBar from "../SideBar/SideBar";
+import NavBar from "../Navbar/NavBar";
+import { Outlet } from "react-router-dom";
 
 export default function MasterLayout() {
-    return (
-        <>
-
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-3 ">
-                        <div className='bg-'>
-                            <SideBar />
-                        </div>
-                    </div>
-                    <div className="col-md-9">
-                        <div className='bg-info'>
-                            <NavBar />
-                        </div>
-                        <div className="content-container">
-                            <Outlet />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <NavBar />
+      <div className="d-flex px-0">
+        <div
+          className=""
+          style={{
+            backgroundColor: "rgb(27 69 53 / 93%)",
+            borderRadius: "0rem 2.625rem 0rem 0rem",
+          }}
+        >
+          <SideBar />
+        </div>
+        <div className="mx-3">
+          <div className=""></div>
+          <div className="content-container bg-warning">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
