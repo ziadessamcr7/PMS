93e1ae8b25ea01c57bf4821cfd7b6d4ef7452580
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2023-12-23 14:18:24 +0200

    add task header container and dynamics location

--- src/Components/Tasks/Tasks.tsx
+++ src/Components/Tasks/Tasks.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+/** @format */
+
+import { useEffect, useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
 
 export default function Tasks() {
+  const [pageName, setPageName] = useState();
+  const location = useLocation();
+  const { pathname } = location;
+
+  const getCurrentPage = () => {
+    const currentPage = pathname.split("/").slice(2).join("/");
+    return setPageName(currentPage);
+  };
+  useEffect(() => {
+    getCurrentPage();
+  }, []);
   return (
-    <div>Tasks</div>
-  )
+    <Container>
+      <Row className="justify-content-between">
+        <Col>
+          <h2 className="text-capitalize">{pageName}</h2>
+        </Col>
+        <Col className="text-end">
+          <Button variant="warning" size="lg" className="text-white ">
+            + Add New Task
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
