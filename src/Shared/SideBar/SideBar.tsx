5230933bde7f30a25516de1994abb6b82721351a
ziad elsharkawy <ziadessamcr7@gmail.com>
2023-12-29 21:24:01 +0200

    linking change-password with sidebar

--- src/Shared/SideBar/SideBar.tsx
+++ src/Shared/SideBar/SideBar.tsx
@@ -3,9 +3,16 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { Modal } from "react-bootstrap";
+import ChangePassword from "../../Components/ChangePassword/ChangePassword";
 
 export default function SideBar() {
   const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);    // to open and close modal
+  const handleShow = () => setShow(true);
 
   const handleToggle = () => {
     setIsCollapsed(!isCollapsed);
@@ -22,62 +29,73 @@ export default function SideBar() {
   }
 
   return (
-    <div className="side-bar">
-      <Sidebar
-        collapsed={isCollapsed}
-        backgroundColor="#0E382F"
-        className="vh-100 text-white"
-      >
-        <Menu className="">
-          <MenuItem
-            onClick={handleToggle}
-            icon={<i className="fa fa-bars d-block"></i>}
-          ></MenuItem>
-          <MenuItem
-            title="dashboard"
-            icon={<i className="fa fa-home"></i>}
-            component={<Link to="/dashboard" />}
-          >
-            {" "}
-            Home
-          </MenuItem>
-          <MenuItem
-            title="users list"
-            icon={<i className="fa fa-users"></i>}
-            component={<Link to="/dashboard/users" />}
-          >
-            {" "}
-            Users
-          </MenuItem>
-          <MenuItem
-            title="recipes"
-            icon={<i className="fa-solid fa-sheet-plastic"></i>}
-            component={<Link to="/dashboard/projects" />}
-          >
-            Projects
-          </MenuItem>
-          <MenuItem
-            title="categories"
-            icon={<i className="fa-solid fa-list-check"></i>}
-            component={<Link to="/dashboard/tasks" />}
-          >
-            Tasks
-          </MenuItem>
-          <MenuItem
-            title="change Password"
-            icon={<i className="fa-solid fa-unlock"></i>}
-          >
-            Change Password
-          </MenuItem>
-          <MenuItem
-            title="logout"
-            onClick={logout}
-            icon={<i className="fa-solid fa-right-from-bracket"></i>}
-          >
-            Logout
-          </MenuItem>
-        </Menu>
-      </Sidebar>
-    </div>
+
+
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Body> <ChangePassword /> </Modal.Body>
+      </Modal>
+
+      <div className="side-bar">
+        <Sidebar
+          collapsed={isCollapsed}
+          backgroundColor="#0E382F"
+          className="vh-100 text-white"
+        >
+          <Menu className="">
+            <MenuItem
+              onClick={handleToggle}
+              icon={<i className="fa fa-bars d-block"></i>}
+            ></MenuItem>
+            <MenuItem
+              title="dashboard"
+              icon={<i className="fa fa-home"></i>}
+              component={<Link to="/dashboard" />}
+            >
+              {" "}
+              Home
+            </MenuItem>
+            <MenuItem
+              title="users list"
+              icon={<i className="fa fa-users"></i>}
+              component={<Link to="/dashboard/users" />}
+            >
+              {" "}
+              Users
+            </MenuItem>
+            <MenuItem
+              title="recipes"
+              icon={<i className="fa-solid fa-sheet-plastic"></i>}
+              component={<Link to="/dashboard/projects" />}
+            >
+              Projects
+            </MenuItem>
+            <MenuItem
+              title="categories"
+              icon={<i className="fa-solid fa-list-check"></i>}
+              component={<Link to="/dashboard/tasks" />}
+            >
+              Tasks
+            </MenuItem>
+            <MenuItem
+              title="change Password"
+              icon={<i className="fa-solid fa-unlock"></i>}
+              onClick={handleShow} >
+              Change Password
+            </MenuItem>
+            <MenuItem
+              title="logout"
+              onClick={logout}
+              icon={<i className="fa-solid fa-right-from-bracket"></i>}
+            >
+              Logout
+            </MenuItem>
+          </Menu>
+        </Sidebar>
+      </div>
+
+    </>
+
+
   );
 }
