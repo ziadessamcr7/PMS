f8aba9f78bcfc347a9a62a28a1c932b0f9fb48ba
ziad elsharkawy <ziadessamcr7@gmail.com>
2023-12-31 17:06:28 +0200

    dashboard admin edit

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -46,63 +46,74 @@ export default function Dashboard() {
 
 
   return (
-    <section>
-      <header className='dashboard-head mt-3 p-4 text-white d-flex align-items-center rounded-3'>
+    <section style={{ marginTop: '20px' }}>
+
+
+      {userRole == 'Manager' ? <>
+        <header className='dashboard-head p-4 text-white d-flex align-items-center rounded-3'>
+          <div>
+            <h1 className='fw-light'>Welcome upskilling Admin</h1>
+            <h1 className='mt-3 fw-light'>You can add project and assign tasks to your team</h1>
+          </div>
+
+        </header>
+
         <div>
-          <h1 className='fw-light'>Welcome upskilling</h1>
-          <h1 className='mt-3 fw-light'>You can add project and assign tasks to your team</h1>
-        </div>
-
-      </header>
-
-      <div>
-        <div className="row mt-3">
-          <div className="col-md-6 ">
-            <h3>Tasks</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, delectus?</p>
-            <div className="row">
-              <div className="col-md-4 text-center">
-                <div className='bg-dark-subtle p-3 rounded-4'>
-                  <h6>ToDo</h6>
-                  <span> {ToDoCount} </span>
+          <div className="row mt-3">
+            <div className="col-md-6 py-3" style={{ backgroundColor: '#F8F9FB' }}>
+              <h5 className='bordr-lft ps-3' >Tasks</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, delectus?</p>
+              <div className="row">
+                <div className="col-md-4 text-center">
+                  <div className=' p-3 rounded-4' style={{ backgroundColor: '#E5E6F4' }}>
+                    <h6>ToDo</h6>
+                    <span> {ToDoCount} </span>
+                  </div>
                 </div>
-              </div>
-              <div className="col-md-4 text-center">
-                <div className='bg-dark-subtle p-3 rounded-4'>
-                  <h6>In progress</h6>
-                  <span> {ProgressCount} </span>
+                <div className="col-md-4 text-center">
+                  <div className='p-3 rounded-4' style={{ backgroundColor: '#F4F4E5' }}>
+                    <h6>In progress</h6>
+                    <span> {ProgressCount} </span>
+                  </div>
                 </div>
-              </div>
-              <div className="col-md-4 text-center">
-                <div className='bg-dark-subtle p-3 rounded-4'>
-                  <h6>Done</h6>
-                  <span> {DoneCount} </span>
+                <div className="col-md-4 text-center">
+                  <div className='p-3 rounded-4' style={{ backgroundColor: '#F4E5ED' }}>
+                    <h6>Done</h6>
+                    <span> {DoneCount} </span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="col-md-6 ">
-            <h3>Users</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur.</p>
-            <div className="row">
-              <div className="col-md-5 text-center">
-                <div className='bg-dark-subtle p-3 rounded-4'>
-                  <h6>Active</h6>
-                  <span> {activeCount} </span>
+            <div className="col-md-6 bg-white py-3">
+              <h5 className='bordr-lft ps-3' >Users</h5  >
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur.</p>
+              <div className="row">
+                <div className="col-md-5 text-center">
+                  <div className='p-3 rounded-4'>
+                    <h6>Active</h6>
+                    <span> {activeCount} </span>
+                  </div>
                 </div>
-              </div>
-              <div className="col-md-5 text-center">
-                <div className='bg-dark-subtle p-3 rounded-4'>
-                  <h6>De-active</h6>
-                  <span> {deActiveCount} </span>
+                <div className="col-md-5 text-center">
+                  <div className='p-3 rounded-4'>
+                    <h6>De-active</h6>
+                    <span> {deActiveCount} </span>
+                  </div>
                 </div>
-              </div>
 
+              </div>
             </div>
           </div>
+
+        </div></> : <header className='dashboard-head mt-3 p-4 text-white d-flex align-items-center rounded-3'>
+        <div>
+          <h1 className='fw-light'>Welcome upskilling User</h1>
+          <h1 className='mt-3 fw-light'>You can view and control your tasks</h1>
         </div>
 
-      </div>
+      </header>}
+
+
 
 
     </section>
